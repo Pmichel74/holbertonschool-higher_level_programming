7c11525f895ce73cd0a39e19a997728d4cfde1ae
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-14 10:37:19 +0100

    task0

---
+++ 0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+# Vérifiez si le nombre est positif, négatif ou zéro et imprimez le résultat
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")

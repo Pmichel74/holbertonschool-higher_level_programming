b0b8c1f04c1451d578459c6a2d4cfbd907feadc1
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-14 19:21:07 +0100

    task9

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,8 +6,7 @@ def uppercase(str):
     for i in str:
         # Check if the character is lowercase (ASCII range 97 to 122)
         if ord(i) >= 97 and ord(i) <= 122:
-            # Convert lowercase character to uppercase
-            # by subtracting 32 from its ASCII value
+            # Convert lowercase character to uppercaby subtracting 32 from its ASCII value
             i = chr(ord(i) - 32)
         # Print the character without a newline at the end
         print("{}".format(i), end="")
---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    # Get the last digit using modulo and abs() for negative numbers
+    last_digit = abs(number) % 10
+
+    # Print the last digit without newline and return it
+    print(last_digit, end="")
+    return last_digit

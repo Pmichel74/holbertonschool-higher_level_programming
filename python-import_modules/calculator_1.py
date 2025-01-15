2b018cf19f4bf9ad7fce1d7c30b3ce4260d7bd58
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-15 15:14:03 +0100

    task1

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Module calculator_1
+Provides basic arithmetic operations: add, sub, mul, div
+"""
+
+
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return a + b
+
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return a - b
+
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return a * b
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)

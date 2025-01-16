6c727856f068f5cf53fa14257cc1269fa18cbe08
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-16 10:11:32 +0100

    push all task

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 # Prints all numbers from 0 to 98 in decimal and in hexadecimal
-print("\n".join("{} = 0x{:x}".format(i, i) for i in range(99)))
+for i in range(99):
+    print("{} = 0x{:x}".format(i, i))

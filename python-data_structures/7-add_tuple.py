12308ba07105e6bdaca96f2939c2b038dcb4a957
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-20 19:16:16 +0100

    task7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,12 +6,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
         if len(tuple_a) == 0:
             tuple_a = 0, 0
         else:
-            tuple_a = tuple_a[0], 0# tuple_a[0] = 1 (premier élément)
-                                   # On ajoute 0 comme deuxième élément
+            tuple_a = tuple_a[0], 0  # tuple_a[0] = 1 (premier élément)
+    # On ajoute 0 comme deuxième élément
     if len(tuple_b) < 2:
-        tuple_b = 0, 0
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
     else:
         tuple_b = tuple_b[0], 0
 
-    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])

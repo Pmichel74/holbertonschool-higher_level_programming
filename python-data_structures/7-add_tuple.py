7bf39b0262634bc566d92d31f6d6c60fddafafc5
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-20 19:26:50 +0100

    task7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     """Add 2 tuples."""
-    
+
     # Normaliser tuple_a à 2 éléments
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
@@ -17,5 +17,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
-    
+
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])

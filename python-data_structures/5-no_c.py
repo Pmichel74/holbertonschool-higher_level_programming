c1c1f61d1b41fec668d01df17a8a0eabf5c31a9e
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-20 14:51:21 +0100

    task5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     """Remove all characters c and C from a string."""
-    
+
     result = []
     for char in my_string:
         if char != 'c' and char != 'C':

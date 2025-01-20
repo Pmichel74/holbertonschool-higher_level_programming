b863d521759f9f88ec34456d78f24cde7e69b8da
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-20 20:08:45 +0100

    task8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     """ returns a tuple with the length of a string and its first character"""
-    if sentence == 0:
+    if sentence == "":
         return (None)
 
     return (len(sentence), sentence[0])

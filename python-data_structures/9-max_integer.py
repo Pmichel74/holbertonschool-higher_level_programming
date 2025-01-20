f4f49ddcc3ac6778016b02dc637d3f8566adc447
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-20 20:21:16 +0100

    task8

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """finds the biggest integer of a list"""
+
+    if not my_list:  # vérifie si la liste est vide
+        return None
+    
+    max_num = my_list[0]  # initialise avec le premier élément
+    for i in range(len(my_list)):
+        if my_list[i] > max_num:
+            max_num = my_list[i]
+    
+    return max_num

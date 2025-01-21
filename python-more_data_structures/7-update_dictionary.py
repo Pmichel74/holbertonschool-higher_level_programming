ae024400625be4f23d9a27a8ce87cf542300bb2c
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-21 20:19:24 +0100

    task7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,16 @@
+#!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    """Replace or add key/value pairs in a dictionary."""
+    """Replace or add key/value in a dictionary.
+
+    Args:
+        a_dictionary: the dictionary to modify
+        key: string key to update or add
+        value: value to associate with key
+    Returns:
+        The modified dictionary
+    """
+
+    # - Update the value if key exists
+    # - Create a new key/value pair if key doesn't exist
     a_dictionary[key] = value
     return (a_dictionary)

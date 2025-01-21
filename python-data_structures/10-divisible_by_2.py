d3489e5fcec954040293f4a85f6fb770b682d1a7
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-21 11:06:14 +0100

    task11

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -5,8 +5,8 @@ def divisible_by_2(my_list=[]):
     multi = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            multi.append(True)
+            multi.append(True)   # Si pair
         else:
-            multi.append(False)
+            multi.append(False)  # Si impair
 
     return (multi)

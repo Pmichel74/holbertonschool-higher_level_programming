43fa8818ef4714534795ab417b128f4caa4ea0d6
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-21 20:28:26 +0100

    task8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,7 @@
 def simple_delete(a_dictionary, key=""):
     """Delete a key in a dictionary."""
     # Check if key exists in dictionary
-    if key in a_dictionary:
+    if key in a_dictionary is not None:
         # Delete the key if it exists
         del a_dictionary[key]
     return (a_dictionary)

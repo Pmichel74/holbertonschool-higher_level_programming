49f3b25fa05a2d02d691f90449440342c0ccded2
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-21 19:03:21 +0100

    task4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -5,4 +5,3 @@ def only_diff_elements(set_1, set_2):
     # Returns elements that are in either set_1 or set_2, but not in both
     # Example: {1,2,3} ^ {3,4,5} = {1,2,4,5}
     return (set_1 ^ set_2)
-    

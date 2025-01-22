1c394a7a889a45e72851ec9300d7f632b2c1e291
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-22 20:05:04 +0100

    task0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -10,12 +10,19 @@ def safe_print_list(my_list=[], x=0):
     Returns:
         The number of elements printed.
     """
+    # Counter for successfully printed elements
     ret = 0
+    
+    # Try to print x elements from the list
     for i in range(x):
         try:
+            # Attempt to print current element and increment counter
             print("{}".format(my_list[i]), end="")
             ret += 1
         except IndexError:
+            # Break loop if index is out of range
             break
+    
+    # Print newline after all elements
     print("")
     return (ret)

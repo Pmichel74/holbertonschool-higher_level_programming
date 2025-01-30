f01322deda91b6e9c37f334ad8b98317af482e05
Pmichel74 <patrick.yann.michelgmail.com>
2025-01-30 19:39:56 +0100

    task8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -60,14 +60,32 @@ class Rectangle:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Return the Rectangle with the greater area.
+
+        Args:
+            rect_1 (Rectangle): The first Rectangle.
+            rect_2 (Rectangle): The second Rectangle.
+        Raises:
+            TypeError: If either of rect_1 or rect_2 is not a Rectangle.
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        return (rect_2)
+    
     def __str__(self):
         """Return the printable representation of the Rectangle.
 
-        Represents the rectangle with the print_symbol character.
+        Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
-            return ""
-
+            return ("")
+        
         rect = []
         for i in range(self.__height):
             for j in range(self.__width):
@@ -75,11 +93,13 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return "".join(rect)
-
+    
     def __repr__(self):
-        """Return string representation of Rectangle for eval()."""
-        return f"Rectangle({self.__width}, {self.__height})"
-
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
+    
     def __del__(self):
         """Print a message for every deletion of a Rectangle."""
         type(self).number_of_instances -= 1

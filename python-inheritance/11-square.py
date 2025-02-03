3206bc9bdcad0c5cdab18918647f518331059199
Pmichel74 <patrick.yann.michelgmail.com>
2025-02-03 20:43:20 +0100

    task11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -15,3 +15,11 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return f"[Square] {self.__size}/{self.__size}"

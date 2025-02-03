bcea64e6d24221ce8d68b091bea0a686d3d7487a
Pmichel74 <patrick.yann.michelgmail.com>
2025-02-03 20:40:59 +0100

    task11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-# Description: Class Square that inherits from Rectangle
-
 """Defines a Rectangle subclass Square."""
-# Import Rectangle class from module 9-rectangle
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -15,9 +12,6 @@ class Square(Rectangle):
         Args:
             size (int): The size of the new square.
         """
-        # Validate size parameter using inherited method
         self.integer_validator("size", size)
-        # Initialize the parent class with size for both width and height
-        super().__init__(size, size) # equivalent : Rectangle.___init__(size, size)
-        # Set private size attribute
+        super().__init__(size, size)
         self.__size = size

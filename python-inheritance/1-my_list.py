837ce5edb3df5c530398c3aaf0ca9bff4865f053
Pmichel74 <patrick.yann.michelgmail.com>
2025-02-03 16:30:30 +0100

    task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,6 +7,10 @@ Defines an inherited list class MyList.
 class MyList(list):
     """A subclass of list that can print itself sorted."""
 
+    def __init__(self, iterable=[]):
+        """Initializes the object with an optional iterable."""
+        super().__init__(iterable)
+
     def print_sorted(self):
         """Prints the list in sorted (ascending) order."""
         print(sorted(self))

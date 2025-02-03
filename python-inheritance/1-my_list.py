172cac9cc2c410eae1da4bc190d0fa15b9306846
Pmichel74 <patrick.yann.michelgmail.com>
2025-02-03 16:09:58 +0100

    task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,13 +3,12 @@
 contains the MyList class
 """
 
-class Mylist(list):
+class MyList(list):
     """a subclass of list"""
     def __init__(self):
         """initializes the object"""
         super().__init__()
 
-
-def print_sorted(self):
-    """prints the sorted list"""
-    print(sorted(self))
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))

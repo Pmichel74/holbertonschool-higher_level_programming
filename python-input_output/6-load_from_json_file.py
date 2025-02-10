866bf03eb16d4c9ef8edbff28343ee7047bb4d47
Pmichel74 <patrick.yann.michelgmail.com>
2025-02-10 11:37:03 +0100

    task6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a JSON file-reading function."""
+import json
+
+def load_from_json_file(filename):
+    """Create a Python object from a JSON file.
+
+    Args:
+        filename (str): The name of the file to read from.
+    Returns:
+        The Python object created from the JSON file.
+    """
+    # Ouvre le fichier en mode lecture
+    with open(filename) as f:
+        # Charge le contenu JSON du fichier et le convertit en objet Python
+        return json.load(f)

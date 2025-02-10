a6c268e77cd64ad796b512b8fe590d7aea1447e5
Pmichel74 <patrick.yann.michelgmail.com>
2025-02-10 15:38:28 +0100

    updates

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# This script demonstrates string formatting with a float number
 
-number = 3.14159
+number = 3.14159  # Assign a float value to variable 'number'
+# Print 'Float:' followed by number formatted to 2 decimal places using format()
 print("Float: {:.2f}".format(number))

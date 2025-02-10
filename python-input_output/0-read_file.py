e1f1837a62ecdde55ed66e1413658d0ee8b6c9f5
Pmichel74 <patrick.yann.michelgmail.com>
2025-02-10 10:18:37 +0100

    task0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    # Ouvre le fichier en mode lecture avec encodage UTF-8
+    with open(filename, encoding="utf-8") as f:
+        # Lit le contenu du fichier et l'affiche sur stdout
+        print(f.read())

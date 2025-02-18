789b7e58474d7509feafa6e9ee80d11ce4fcb26c
Pmichel74 <patrick.yann.michelgmail.com>
2025-02-18 19:13:53 +0100

    task02

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,54 +1,49 @@
 #!/usr/bin/python3
-"""Module that defines a function to fetch posts from an API"""
-import json
-import requests
+"""Defines function that fetches posts"""
 import csv
+import requests
 
 
 def fetch_and_print_posts():
-    """Fetches and prints posts from JSONPlaceholder"""
+    """function fetches and prints"""
     url = "https://jsonplaceholder.typicode.com/posts"
 
     try:
         res = requests.get(url)
-        res.raise_for_status()
-        json_data = res.json()
+        res.raise_for_status()  # Raise an exception for HTTP errors
     except requests.RequestException as e:
         print(f"Failed to retrieve data: {e}")
         return
 
     print("Status Code: {}".format(res.status_code))
 
-    # Print titles only if request was successful
-    if res.status_code == 200:
+    if res.headers.get("Content-Type") == "application/json; charset=utf-8":
+        json_data = res.json()
         for post in json_data:
             print(post["title"])
 
 
 def fetch_and_save_posts():
-    """Function that fetches posts and saves them to a CSV file"""
+    """
+    Fetches all posts from JSONPlaceholder and saves them in a csv file.
+    """
     url = "https://jsonplaceholder.typicode.com/posts"
-    csvfile = "posts.csv"
 
     try:
         res = requests.get(url)
-        res.raise_for_status()
-        json_data = res.json()
-
-        filtered_data = [{key: post[key] for key in ('id', 'title', 'body')}
-                         for post in json_data]
-
-        with open(csvfile, "w", newline="") as file:
-            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
-            writer.writeheader()
-            writer.writerows(filtered_data)
-
     except requests.RequestException as e:
-        print(f"Request failed: {e}")
+        print(f"Failed to retrieve data: {e}")
         return
-    except json.JSONDecodeError as e:
-        print(f"Failed to parse JSON: {e}")
-        return
-    except IOError as e:
-        print(f"Failed to write to file: {e}")
-        return
+
+    json_data = res.json()
+    csvfile = "posts.csv"
+    filtered_data = [
+        {key: post[key] for key in ('id', 'title', 'body')}
+        for post in json_data
+    ]
+    headers = ['id', 'title', 'body']
+
+    with open(csvfile, "w", newline="") as file:
+        csv_write = csv.DictWriter(file, fieldnames=headers)
+        csv_write.writeheader()
+        csv_write.writerows(filtered_data)

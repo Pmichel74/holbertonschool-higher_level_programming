dc6d6d4768d257cb49b34eab36307d96e5c9d6e3
Pmichel74 <patrick.yann.michel@gmail.com>
2025-03-04 15:28:53 +0100

    task15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 -- Lists the number of records with the same score in the table second_table.
 -- Records are grouped by score with label number.
 -- Records are ordered by descending score.
-SELECT score, COUNT(*)
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER BY score DESC;

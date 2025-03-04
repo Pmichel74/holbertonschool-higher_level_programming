3322ff0dd58a174baad6877608a573e68158d9b0
Pmichel74 <patrick.yann.michel@gmail.com>
2025-03-04 15:48:28 +0100

    task16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
--- Lists all records of the table second_table having a score greater than 10.
+-- Lists all records of the table second_table having a score greater than 10 and a non-empty name.
 -- Records are ordered by descending score.
-SELECT score, name
+SELECT score, name 
 FROM second_table
-WHERE score > 10
+WHERE score > 10 AND name IS NOT NULL AND name != ''
 ORDER BY score DESC;
